7c43111a55253275969804bbca1f4e95fcd45159
Jacek Małyszko <jmalyszko@fandom.com>
2019-12-23 14:22:06 +0100

    Slack notificaitons to users and the admin channel now have separate tresholds

--- bin/tests/test_notification.py
+++ bin/tests/test_notification.py
@@ -17,16 +17,20 @@ class NotificatorTest(unittest.TestCase):
 
         return response_mock
 
-    @patch('notification.requests')
-    def test_handle_batch_event(self, requests):
-
+    @staticmethod
+    def prepare_config_mock(user_threshold, channel_threshold):
         config = Mock()
-        config.SLACK_ALERT_DATA_THRESHOLD = 100
+        config.SLACK_ALERT_DATA_USER_THRESHOLD = user_threshold
+        config.SLACK_ALERT_DATA_CHANNEL_THRESHOLD = channel_threshold
         config.SLACK_USER_MAPPINGS = {'test': 'mapped_user'}
         config.SLACK_MESSAGE = 'test message'
         config.SLACK_WEBHOOK_URL = 'url'
         config.SLACK_BOT_TOKEN = 'token'
+        return config
 
+    @patch('notification.requests')
+    def test_handle_batch_event_user_and_channel_value_above_threshold(self, requests):
+        config = NotificatorTest.prepare_config_mock(user_threshold=100, channel_threshold=100)
         body = utils.get_content('fixtures/notification_sqs_event.json')
 
         requests.post.side_effect = NotificatorTest.requests_side_effect
@@ -44,6 +48,42 @@ class NotificatorTest(unittest.TestCase):
         requests.post.assert_any_call('https://slack.com/api/chat.postMessage',
                                       headers={'Authorization': 'Bearer token'},
                                       json={'channel': 'test_channel', 'text': 'test message'})
+
+    @patch('notification.requests')
+    def test_handle_batch_event_test_separate_thresholds(self, requests):
+        # we should get only one API call as user threshold is above the actual value and channel threshold is below
+        # please be advised that if slack API will change in the future...
+        # ...this test may start to fail (as more than one requests may be needed)
+        config = NotificatorTest.prepare_config_mock(user_threshold=10000000000000000, channel_threshold=100)
+        body = utils.get_content('fixtures/notification_sqs_event.json')
+
+        requests.post.side_effect = NotificatorTest.requests_side_effect
+        requests.codes.ok = 200
+
+        events = dict(Records=[dict(body=body)])
+
+        sut = Notificator(config)
+        sut.handle_batch_event(events)
+
+        requests.post.assert_any_call('url', json={'text': 'test message', 'link_names': 1})
+        requests.post.assert_called_once()
+
+    @patch('notification.requests')
+    def test_handle_batch_event_user_and_channel_value_below_threshold(self, requests):
+        # we should have no API calls as user and channel thresholds are above the actual value
+        config = NotificatorTest.prepare_config_mock(user_threshold=10000000000000000, channel_threshold=10000000000000)
+        body = utils.get_content('fixtures/notification_sqs_event.json')
+
+        requests.post.side_effect = NotificatorTest.requests_side_effect
+        requests.codes.ok = 200
+
+        events = dict(Records=[dict(body=body)])
+
+        sut = Notificator(config)
+        sut.handle_batch_event(events)
+
+        requests.post.assert_not_called()
+
 
 if __name__ == '__main__':
     unittest.main()
